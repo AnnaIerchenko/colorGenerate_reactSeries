36c9dd0f6a3804db7af44fea9e42b913126d50bb
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-11-05 09:46:28 -0800

    copy color to clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({ rgb, weight, index, hexColor}) => {
@@ -7,12 +7,26 @@ const SingleColor = ({ rgb, weight, index, hexColor}) => {
   const bcg = rgb.join(',')
   const hex = rgbToHex(...rgb)
   const hexValue = `#${hexColor}`
+  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [alert])
 
   return (
     <article className={`color ${index > 10 && 'color-light'}`} 
-      style={{backgroundColor:`rgb(${bcg})`}}>
+      style={{backgroundColor:`rgb(${bcg})`}}
+      // copy to clipboard
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hexValue)
+      }}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   )
 }
